e969c37efc6fd6639929f6a5ba280fa0040b1178
Karthikeyan Yegappan <karthikayen@gmail.com>
2019-08-29 15:47:29 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import routes from './Routes';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Bulma a modular css framework with no js </h1>
+      <Navigation />
+      <Router>
+        {routes.map(route => (
+          <Route
+            path={route.path}
+            render={route.component}
+          />
+        ))}
+      </Router>
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import App from './App';
+import Layouts from './Components/Layouts';
+
+const routes = [
+    {
+        path: "/home",
+        component: App
+    },
+    {
+        path: "/layouts",
+        component: Layouts
+    }
+];
+
+export default routes;
