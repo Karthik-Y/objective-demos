bcc0888b9865544dd2499d30d3be1d27e448c24e
Karthikeyan Yegappan <karthikayen@gmail.com>
2019-09-03 18:03:28 +0100

    Add node sass and introduce bulma sass

--- Sept5th2019/Bulma/bulma-demo/src/App.js
+++ Sept5th2019/Bulma/bulma-demo/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Navigation from './Components/Navigation';
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import routes from './Routes';
-
+import 'bulma/bulma.sass';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Bulma a modular css framework with no js </h1>
       <Navigation />
       <Router>
         {routes.map(route => (
