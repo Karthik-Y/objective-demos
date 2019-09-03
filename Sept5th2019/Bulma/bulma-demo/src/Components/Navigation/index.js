0465b600324abe4a6580ca9b2bf8f1083083239b
Karthikeyan Yegappan <karthikayen@gmail.com>
2019-09-03 17:05:36 +0100

    Add bulma demo scripts

---
+++ Sept5th2019/Bulma/bulma-demo/src/Components/Navigation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li> <a href="/"> Home </a> </li>
+                <li> <a href="/layouts"> Layout </a> </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
